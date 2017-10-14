bbcae2d415d3adaa4e07035a103c2a982fd47f64
YOSHIOKA Takuma <tashioka.256@gmail.com>
2017-10-15 01:09:22 +0900

    Add helper utilities to check configs and update mastodon

---
+++ lo48576/scripts/update.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+cd "$(dirname "$0")"
+cd ../../
+
+# If you don't customize Dockerfile, no need to `build` containers.
+# Everything required is prepared by `pull`.
+docker-compose -f docker-compose.yml -f docker-compose.override.10-network.yml -f docker-compose.override.20-reverse-proxy.yml pull
+# `docker-compose ... build` here, if you need.
+docker-compose -f docker-compose.yml -f docker-compose.override.10-network.yml run --rm web bin/rails db:migrate
+docker-compose -f docker-compose.yml -f docker-compose.override.10-network.yml run --rm web bin/rails assets:precompile
